6b31632a39b0d3a863a583b21a62f748db211d5f
kangwook <rlarkddnr1686@naver.com>
2024-04-03 19:51:15 +0900

    [config] : 기본 의존성 추가 및 Jasypt 암호화 설정

---
+++ src/main/java/com/evertrip/EvertripApplication.java
@@ -0,0 +1,13 @@
+package com.evertrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvertripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvertripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evertrip/EvertripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evertrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvertripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
