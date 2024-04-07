a71b8487e689ef338110d2cb7ff7ff2b5b375f6f
kangwook <rlarkddnr1686@naver.com>
2024-04-07 17:31:01 +0900

    [feat] : API 공통 응답 형식 지정 및 RestControllerAdvice를 이용한 API 예외 처리 구현

---
+++ src/main/java/com/evertrip/api/exception/ErrorCode.java
@@ -0,0 +1,26 @@
+package com.evertrip.api.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    // COMMON
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Permission is invalid"),
+    ACCESS_FORBIDDEN(HttpStatus.FORBIDDEN, "Access forbidden"),
+    STATUS_VALUE_NOT_FOUND(HttpStatus.NOT_FOUND, "status value not founded"),
+    INVALID_VALUE(HttpStatus.FORBIDDEN, "Invalid value"),
+    INVALID_ENUM_VALUE(HttpStatus.BAD_REQUEST, "Invalid enum value"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+    CRYPT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Crypt error");
+
+
+
+
+
+    private HttpStatus status;
+    private String message;
+}
