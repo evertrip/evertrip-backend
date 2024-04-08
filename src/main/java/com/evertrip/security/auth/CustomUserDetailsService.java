e20605dc689a293ccb2f2e3b1a938ca8e13dfb88
kangwook <rlarkddnr1686@naver.com>
2024-04-08 12:05:37 +0900

    [feat] : 시큐리티 인증에 사용할 사용자 정의 UserDetail 설정 및 CustomAuthenticationProvider 지정 #5

---
+++ src/main/java/com/evertrip/security/auth/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.evertrip.security.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+// UserDetailsService를 implements하고 UserRepository를 주입받는다.
+// loadUserByUsername 메소드를 오버라이드해서 로그인 시에 DB에서 유저정보와 권한정보를 가져오게 된다.
+// 해당 정보를 기반으로 userdetails.User 객체를 생성해서 리턴한다.
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    public CustomUserDetailsService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(final String email) {
+        return memberRepository.findByEmail(email)
+                .map(member ->  new MemberDetails(member))
+                .orElseThrow(() -> new UsernameNotFoundException(email + " -> 데이터베이스에서 찾을 수 없습니다."));
+    }
+}
---
+++ src/main/java/com/evertrip/security/auth/MemberDetails.java
@@ -0,0 +1,55 @@
+package com.evertrip.security.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class MemberDetails implements UserDetails {
+    private Member member;
+
+    public Member getMember() {
+        return this.member;
+    }
+
+    public MemberDetails(Member member) {
+        this.member = member;
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getId().toString();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
