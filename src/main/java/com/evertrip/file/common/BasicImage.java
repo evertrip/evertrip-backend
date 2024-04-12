64df8714e0100c759500613e509aa2a87a5927a8
kangwook <rlarkddnr1686@naver.com>
2024-04-12 10:08:43 +0900

    [feat] : 기본 사용자 이미지 설정  #7

--- src/main/java/com/evertrip/file/common/BasicImage.java
+++ src/main/java/com/evertrip/file/common/BasicImage.java
@@ -7,9 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum BasicImage {
 
-    BASIC_USER_IMAGE("https://evertrip-bucket.s3.ap-northeast-2.amazonaws.com/basic_user_image.jpg"),
-    BASIC_DOG_IMAGE("https://evertrip-bucket.s3.ap-northeast-2.amazonaws.com/basic_dog_image.jpg"),
-    BASIC_JOB_POST_IMAGE("https://evertrip-bucket.s3.ap-northeast-2.amazonaws.com/basic_board_image.jpg");
+    BASIC_USER_IMAGE("https://ever-trip-bucket.s3.ap-northeast-2.amazonaws.com/basic_user_image.jpg");
 
     private String path;
 }
