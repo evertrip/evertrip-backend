e47a48a7c7730902df6e24f3d1389a842b0af626
kangwook <rlarkddnr1686@naver.com>
2024-04-25 14:33:10 +0900

    [feat] : 회원 프로필 조회 테스트 케이스 작성 #7

--- src/test/java/com/evertrip/member/controller/MemberTest.java
+++ src/test/java/com/evertrip/member/controller/MemberTest.java
@@ -1,7 +1,9 @@
 package com.evertrip.member.controller;
 
+import com.evertrip.member.dto.response.MemberProfileResponseDto;
 import com.evertrip.member.entity.Member;
 import com.evertrip.member.entity.Role;
+import com.evertrip.member.repository.MemberProfileRepository;
 import com.evertrip.member.repository.MemberRepository;
 import com.evertrip.member.repository.RoleRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +13,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -25,7 +29,12 @@ class MemberTest {
     MemberRepository memberRepository;
 
     @Autowired
+    MemberProfileRepository memberProfileRepository;
+
+    @Autowired
     RoleRepository roleRepository;
+
+    private static String token = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwiZXhwIjoxNzE1NDcyMDE4fQ.sfnp2mG6mhZV7wezNaL1YVvZadMNT8U8mT6wy2FST4-0ZkvQ1xfw2oEE-EbWAgTJQZ7-82HpNs_90RI5gaoYWg";
 
     @Autowired
     MockMvc mockMvc;
@@ -47,7 +56,7 @@ class MemberTest {
 
         //then
         // 일반 사용자의 경우 /admin에 대한 요청에 403 권한 에러가 발생하여야 합니다.
-        mockMvc.perform(get("/admin/test1").header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwiZXhwIjoxNzE1NDcyMDE4fQ.sfnp2mG6mhZV7wezNaL1YVvZadMNT8U8mT6wy2FST4-0ZkvQ1xfw2oEE-EbWAgTJQZ7-82HpNs_90RI5gaoYWg"))
+        mockMvc.perform(get("/admin/test1").header("Authorization", token))
                 .andExpect(status().isForbidden());
 
 
@@ -57,9 +66,34 @@ class MemberTest {
 
         // then
         // /admin에 대한 요청 시 관리자로 권한을 바꿨기 때문에 통과되어야 합니다.
-        mockMvc.perform(get("/admin/test1").header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwiZXhwIjoxNzE1NDcyMDE4fQ.sfnp2mG6mhZV7wezNaL1YVvZadMNT8U8mT6wy2FST4-0ZkvQ1xfw2oEE-EbWAgTJQZ7-82HpNs_90RI5gaoYWg"))
+        mockMvc.perform(get("/admin/test1").header("Authorization", token))
                 .andExpect((status().isOk()));
 
     }
+
+    @DisplayName("회원 프로필 조회 테스트")
+    @Test
+    public void getMemberProfileTest() throws Exception {
+        // given
+
+        // when
+        // pk가 2L인 사용자의 프로필을 조회합니다. 헤더의 토큰은 해당 pk의 사용자 토큰입니다.
+        List<MemberProfileResponseDto> memberProfiles = memberProfileRepository.findMemberProfiles(2L, false);
+        MemberProfileResponseDto memberProfile = memberProfiles.get(0);
+
+
+        // then
+        // jsonPath를 이용하여 컨트롤러가 주는 HTTP 응답 바디에 접근하여 값을 비교하는 코드입니다.
+        mockMvc.perform(get("/members")
+                        .header("Authorization", token))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].memberId").value(memberProfile.getMemberId()))
+                .andExpect(jsonPath("$.content[0].nickName").value(memberProfile.getNickName()))
+                .andExpect(jsonPath("$.content[0].description").value(memberProfile.getDescription()))
+                .andExpect(jsonPath("$.content[0].createdAt").value(memberProfile.getCreatedAt()))
+                .andExpect(jsonPath("$.content[0].updatedAt").value(memberProfile.getUpdatedAt()))
+                .andExpect(jsonPath("$.content[0].profileImage").value(memberProfile.getProfileImage()));
+    }
+
 
 }
