4c8d5e0508c5f2bf730da60b86f54007ca8fc4cf
kangwook <rlarkddnr1686@naver.com>
2024-05-04 21:14:37 +0900

    [feat] : 로그인 관련 redirect 및 쿠키 세팅 수정  #2

--- src/main/java/com/evertrip/oauth/controller/OauthController.java
+++ src/main/java/com/evertrip/oauth/controller/OauthController.java
@@ -1,6 +1,7 @@
 package com.evertrip.oauth.controller;
 
 import com.evertrip.api.response.ApiResponse;
+import com.evertrip.constant.ConstantPool;
 import com.evertrip.constant.ConstantPool.SocialLoginType;
 import com.evertrip.member.dto.response.MemberSimpleResponseDto;
 import com.evertrip.oauth.service.OauthService;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
 
@@ -32,7 +34,7 @@ public class OauthController {
      * 소셜 로그인 요청에 대한 응답 처리(로그인, 회원가입 처리)
      */
     @GetMapping("/auth/{socialLoginType}/callback")
-    public ResponseEntity callback(
+    public RedirectView callback(
             @PathVariable(name="socialLoginType") String type,
             @RequestParam(name="code") String code,
             HttpServletRequest request) throws IOException {
@@ -40,8 +42,8 @@ public class OauthController {
         System.out.println(">> 소셜 로그인 API 서버로부터 받은 code :"+ code);
         HttpHeaders httpHeaders = new HttpHeaders();
         SocialLoginType socialLoginType= SocialLoginType.valueOf(type.toUpperCase());
-        Long memberId=Long.parseLong(oauthService.oauthLogin(socialLoginType,code,httpHeaders,request));
-        return new ResponseEntity<>(ApiResponse.successOf(new MemberSimpleResponseDto(memberId)),httpHeaders ,HttpStatus.OK);
+        oauthService.oauthLogin(socialLoginType,code,httpHeaders,request);
+        return new RedirectView(ConstantPool.FRONT_LOCAL_HOST);
     }
 
 }
