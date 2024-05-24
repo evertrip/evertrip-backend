5193d6b28d4e3b96a780389018e1d0a538e3ad04
hyeminLapTop <gpals5987@gmail.com>
2024-05-24 14:55:50 +0900

    [feat] search LikeMembers #12

---
+++ src/main/java/com/evertrip/like/repository/LikeRepository.java
@@ -0,0 +1,18 @@
+package com.evertrip.like.repository;
+
+import com.evertrip.like.dto.response.LikeMembersResponseDto;
+import com.evertrip.like.entity.PostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface LikeRepository extends JpaRepository<PostLike, Long> {
+
+    @Query("SELECT l.memberId FROM PostLike  l WHERE l.postId = :postId group by l.memberId")
+    List<Long> findLikeMembersByPostId(@Param("postId") Long postId);
+}
+
+
+
